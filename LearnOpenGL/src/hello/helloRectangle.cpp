1b518a4d0677ca7d49221919165b90e6e67a9aba
Jun <oren.ccl@gmail.com>
2023-03-14 16:45:24 +0800

    [!] Hello two different triangle

--- LearnOpenGL/src/hello/helloRectangle.cpp
+++ LearnOpenGL/src/hello/helloRectangle.cpp
@@ -110,7 +110,7 @@ int main() {
 		0.5f,  0.5f, 0.0f,  // top right
 		0.5f, -0.5f, 0.0f,  // bottom right
 		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		-0.5f,  0.5f, 0.0f,   // top left 
 	};
 
 	unsigned int indices[] = {
